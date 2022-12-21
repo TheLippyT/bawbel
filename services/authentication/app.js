e26bdcc1d2791884dfc081977a5aa8e66a6ffc9f
Dean Martodikromo <dmartodikromo@qualogy.com>
2022-12-21 11:51:40 -0300

    basic authentication service. will requires a mongodb database

---
+++ services/authentication/app.js
@@ -0,0 +1,111 @@
+require("dotenv").config();
+require("./config/database").connect();
+import express, { json } from "express";
+import { hash, compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { findOne, create } from "./model/user";
+import auth from "./middleware/auth";
+
+const app = express();
+
+app.use(json({ limit: "50mb" }));
+
+app.post("/register", async (req, res) => {
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await hash(password, 10);
+
+    // Create user in our database
+    const user = await create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await findOne({ email });
+
+    if (user && (await compare(password, user.password))) {
+      // Create token
+      const token = sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+
+// This should be the last route else any after it won't work
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
+});
+
+export default app;
---
+++ services/authentication/config/database.js
@@ -0,0 +1,21 @@
+import { connect } from "mongoose";
+
+const { MONGO_URI } = process.env;
+
+export function connect() {
+  // Connecting to the database
+  connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+}
